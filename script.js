18c220cdb8ed8df0218f5ee4f693b51b4691b0d0
JartigaRox <00171124@uca.edu.sv>
2025-08-27 19:39:39 -0600

    feat:Ternminar proyecto y arreglar errores

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    
+    window.agregarImagen = function() {
+        if (!document.getElementById('miImagen')) {
+            const img = document.createElement("img");
+            img.src = "imagenes/imagen.jpeg"; 
+            img.alt = "Descripcion de la imagen";
+            img.style.maxWidth = "100%";
+            img.style.height = "auto";
+            img.id = "miImagen";
+            document.body.appendChild(img);
+            const ulDiv = document.querySelector("div ul");
+            if (ulDiv) {
+                ulDiv.appendChild(img);
+            }
+        }
+    }
+
+    window.ordenarContenido = function() {
+        const contenedor = document.querySelector(".contenido");
+        contenedor.style.display = "flex";
+        contenedor.style.flexDirection = "column";
+
+    }
+
+    window.cambiarTitulo = function() {
+        const nuevoTitulo = prompt("Introduce el nuevo título:");
+        if (nuevoTitulo) {
+            document.querySelector("#header h1").textContent = nuevoTitulo;
+        }
+    }
+
+    window.cambiarEstilo = function() {
+        const contenido = document.querySelector(".subDesc");
+        contenido.style.fontFamily = "comic sans ms, sans-serif";
+        contenido.style.color = "red";
+    }
 });
